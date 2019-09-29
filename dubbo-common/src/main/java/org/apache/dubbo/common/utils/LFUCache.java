e54ef65f4d8b9568e9bf5adb502958fdad050d7c
dyuan1 <dyuan1@xiaohongshu.com>
2019-09-29 15:46:07 +0800

    use concurrent collection

--- dubbo-common/src/main/java/org/apache/dubbo/common/utils/LFUCache.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/utils/LFUCache.java
@@ -1,28 +1,35 @@
 package org.apache.dubbo.common.utils;
 
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * LFUCache
+ * @author  winter
+ * @date    2019/9/12
+ */
 public class LFUCache {
-    private HashMap<Object, Node> kvMap;
-    private HashMap<Object, LinkedHashSet<Node>> frequencyMap;
+    private ConcurrentHashMap<Object, Node> cacheMap;
+    private ConcurrentHashMap<Object, Set<Node>> frequencyMap;
     private int minFrequency;
     private int capacity;
 
     public LFUCache(int capacity) {
         this.capacity = capacity;
-        kvMap = new HashMap<>();
-        frequencyMap = new HashMap<>();
+        cacheMap = new ConcurrentHashMap<>();
+        frequencyMap = new ConcurrentHashMap<>();
         minFrequency = 1;
     }
 
     public Object get(Object key) {
-        if (!kvMap.containsKey(key)) {
+        if (!cacheMap.containsKey(key)) {
             return null;
         } else {
-            Node node = kvMap.get(key);
+            Node node = cacheMap.get(key);
             updateNode(node);
             return node.value;
         }
@@ -32,8 +39,8 @@ public class LFUCache {
         if (capacity <= 0) {
             return;
         }
-        if (kvMap.containsKey(key)) {
-            Node node = kvMap.get(key);
+        if (cacheMap.containsKey(key)) {
+            Node node = cacheMap.get(key);
             if (node.value != value) {
                 node.value = value;
             }
@@ -42,38 +49,38 @@ public class LFUCache {
         } else {
             Node node = new Node(key, value, 1);
             if (!frequencyMap.containsKey(node.frequency)) {
-                LinkedHashSet<Node> newLink = new LinkedHashSet<>();
+                Set<Node> newLink = Collections.synchronizedSet(new LinkedHashSet<>());
                 newLink.add(node);
                 frequencyMap.put(node.frequency, newLink);
             } else {
-                LinkedHashSet<Node> newLink = frequencyMap.get(node.frequency);
+                Set<Node> newLink = frequencyMap.get(node.frequency);
                 newLink.add(node);
                 frequencyMap.put(node.frequency, newLink);
             }
-            kvMap.put(key, node);
+            cacheMap.put(key, node);
         }
-        if (kvMap.size() > capacity) {
-            LinkedHashSet<Node> minFrequencyList = frequencyMap.get(minFrequency);
+        if (cacheMap.size() > capacity) {
+            Set<Node> minFrequencyList = frequencyMap.get(minFrequency);
             Node evict = minFrequencyList.iterator().next();
             minFrequencyList.remove(evict);
-            kvMap.remove(evict.key);
+            cacheMap.remove(evict.key);
         }
         minFrequency = 1;
     }
 
     private void updateNode(Node node) {
-        LinkedHashSet<Node> currentLink = frequencyMap.get(node.frequency);
+        Set<Node> currentLink = frequencyMap.get(node.frequency);
         currentLink.remove(node);
         if (currentLink.isEmpty() && node.frequency == minFrequency) {
             minFrequency++;
         }
         node.frequency++;
         if (frequencyMap.containsKey(node.frequency)) {
-            LinkedHashSet<Node> nextLink = frequencyMap.get(node.frequency);
+            Set<Node> nextLink = frequencyMap.get(node.frequency);
             nextLink.add(node);
             frequencyMap.put(node.frequency, nextLink);
         } else {
-            LinkedHashSet<Node> nextLink = new LinkedHashSet<>();
+            Set<Node> nextLink = Collections.synchronizedSet(new LinkedHashSet<>());
             nextLink.add(node);
             frequencyMap.put(node.frequency, nextLink);
         }
